4f4a66babb0589d2562643d87cb5a73964c59c12
Oleksandr Lisovyk <oleksandr_lisovyk@epam.com>
2016-02-26 16:29:49 +0100

    Moving formula-1 backend from separate app to module

--- app.js
+++ app.js
@@ -37,6 +37,8 @@ if (module === require.main) {
     var host = server.address().address;
     var port = server.address().port;
 
+
+
     console.log('App listening at http://%s:%s', host, port);
   });
   // [END server]
