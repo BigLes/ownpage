fb446d6fcf58b0d062a6251103235ce9ed5c32db
Oleksandr Lisovyk <oleksandr_lisovyk@epam.com>
2016-02-26 16:39:22 +0100

    Configuration update

--- app.js
+++ app.js
@@ -1,26 +1,12 @@
-// Copyright 2015-2016, Google, Inc.
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//    http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
 'use strict';
 
-var express = require('express');
-var path = require('path');
+let express = require('express');
+let path = require('path');
 
-var app = express();
+let FormulaBackEnd = require('./formula1-back-end');
 
+let app = express();
 
-// [START hello_world]
-// Say hello!
 app.get('/', function(req, res) {
   res.status(200).send('Hello, world!');
 });
@@ -28,20 +14,16 @@ app.use(express.static(__dirname + '/F1'));
 app.get('/formula1', function(req, res) {
 	res.sendFile(path.join(__dirname + '/F1/index.html'));
 });
-// [END hello_world]
 
 if (module === require.main) {
-  // [START server]
-  // Start the server
-  var server = app.listen(process.env.PORT || 8080, function () {
-    var host = server.address().address;
-    var port = server.address().port;
+    let server = app.listen(process.env.PORT || 8080, function () {
+        let host = server.address().address;
+        let port = server.address().port;
 
+        new FormulaBackEnd(host);
 
-
-    console.log('App listening at http://%s:%s', host, port);
+        console.log('App listening at http://%s:%s', host, port);
   });
-  // [END server]
 }
 
 module.exports = app;
