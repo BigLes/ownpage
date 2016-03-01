4636107bfdf8ae9b428c45645add23bed9a80880
BigLes <bigles@bigmir.net>
2016-03-01 23:49:29 +0100

    minor changes - change directory

--- app.js
+++ app.js
@@ -10,14 +10,14 @@ let app = express();
 app.get('/', function(req, res) {
   res.status(200).send('Hello, world!');
 });
-app.use(express.static(__dirname + '/F1'));
+app.use(express.static(__dirname + '/formula1-front-end'));
 app.get('/formula1', function(req, res) {
-	res.sendFile(path.join(__dirname + '/F1/index.html'));
+	res.sendFile(path.join(__dirname + '/formula1-front-end/index.html'));
 });
 
 if (module === require.main) {
     let server = app.listen(process.env.PORT || 8080, function () {
-        let host = server.address().address ? server.address().address : '127.0.0.1';
+        let host = server.address().address;
         console.log('Host: ' + host);
         if (host === '::' || !host) {
             host = '127.0.0.1';
