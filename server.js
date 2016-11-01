6cc5a86db2fbded0899a87ec3efa4c9d1bed5481
BigLes <bigles@bigmir.net>
2016-11-01 17:01:45 +0100

    update cors

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ let app = express();
 app.use(function allowCrossDomain(req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
 app.use(express.static(__dirname + '/www'));
